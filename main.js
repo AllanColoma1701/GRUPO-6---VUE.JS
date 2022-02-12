29920818747265e64bb49d647526fb9340fbeadf
Allan Coloma <42362779+AllanColoma1701@users.noreply.github.com>
2022-02-11 22:52:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+const app = Vue.createApp({
+    data (){
+        return{
+            producto: 'Medias', 
+            marca: 'Vue Mastery',
+            imagen: './images/medias_verdes.jpg',
+            detalles:['50% Algodón', '30% Lana', '20% Poliéster'], 
+            variantes: [
+                {id: 2234, color: 'green', imagen: './images/medias_verdes.jpg', cantidad: 50}, 
+                {id: 2235, color: 'blue', imagen:'./images/medias_azules.jpg', cantidad: 0}
+                       ], 
+            carrito: 0, 
+            varianteSeleccionada: 0,
+
+
+
+        }    
+    }, 
+    methods: {
+        añadirCarrito() {
+            this.carrito += 1
+        }, 
+        actualizarVariante(index) {
+            this.varianteSeleccionada = index
+        }
+    }, 
+
+    computed: {
+        titulo() {
+            return this.producto + '' + this.marca
+        }, 
+        imagen() {
+            return this.variantes[this.varianteSeleccionada].imagen
+        }, 
+        enStock() {
+            return this.variantes[this.varianteSeleccionada].cantidad
+        }
+
+    }
+
+})
+
